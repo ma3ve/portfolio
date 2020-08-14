a1fa174c35332efe860c9c68e321353e72a5f334
maeve <mujammilkhan321@gmail.com>
2020-08-14 14:28:16 +0530

    Updates

---
+++ precache-manifest.ec98bef46a68493d6421b00e391e09db.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd2a1fbaa5f3f58e088e13d8f929e29",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "70ed29258681a6b83464",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7fb5f3736043cc7d2c9f",
+    "url": "/portfolio/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "70ed29258681a6b83464",
+    "url": "/portfolio/static/js/2.d45946a5.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/portfolio/static/js/2.d45946a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb5f3736043cc7d2c9f",
+    "url": "/portfolio/static/js/main.bfd2cd6a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7219a99d8eafe73fdfb6600ce13efb4d",
+    "url": "/portfolio/static/media/cpp.7219a99d.svg"
+  },
+  {
+    "revision": "a83558fe001f607c817393bc28a21d03",
+    "url": "/portfolio/static/media/django.a83558fe.svg"
+  },
+  {
+    "revision": "3dbfd3dbe9eb42069a6e77be52ab7f4c",
+    "url": "/portfolio/static/media/ethereum.3dbfd3db.svg"
+  },
+  {
+    "revision": "e04514d95c03243368592d4f508c06ad",
+    "url": "/portfolio/static/media/git.e04514d9.svg"
+  },
+  {
+    "revision": "8f26337943c617347b96cab762234618",
+    "url": "/portfolio/static/media/javascript.8f263379.svg"
+  },
+  {
+    "revision": "540b3988f7dc7aff2b79f7953a05e1d1",
+    "url": "/portfolio/static/media/mysql.540b3988.svg"
+  },
+  {
+    "revision": "a3ad45d8f68a372b2c110398f2e4db85",
+    "url": "/portfolio/static/media/nodejs.a3ad45d8.svg"
+  },
+  {
+    "revision": "9d8653c1296ea4e24003fab09cd0cf12",
+    "url": "/portfolio/static/media/python.9d8653c1.svg"
+  },
+  {
+    "revision": "4c28ca5b51f9eeb901eb0339ad4ab377",
+    "url": "/portfolio/static/media/reactjs.4c28ca5b.svg"
+  }
+]);
